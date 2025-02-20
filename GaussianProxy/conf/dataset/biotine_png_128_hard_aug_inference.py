1694919381abe4f07aa47086ea5944ddc800480c
Thomas Boyer <tboyer@bio.ens.psl.eu>
2025-02-20 11:12:52 +0100

    add hard aug inference version and remove old configs

--- GaussianProxy/conf/dataset/biotine_png_128_hard_aug_inference.py
+++ GaussianProxy/conf/dataset/biotine_png_128_hard_aug_inference.py
@@ -1,30 +1,32 @@
-from torchvision.transforms import Compose, Normalize, Resize
+import torch
+from torchvision.transforms import Compose, ConvertImageDtype, Normalize, Resize
 
 from GaussianProxy.conf.training_conf import DataSet, DatasetParams
-from GaussianProxy.utils.data import NumpyDataset
+from GaussianProxy.utils.data import ImageDataset
 
 DEFINITION = 128
 NUMBER_OF_CHANNELS = 3
 
 transforms = Compose(
     transforms=[
-        Normalize(mean=[0.5] * NUMBER_OF_CHANNELS, std=[0.5] * NUMBER_OF_CHANNELS),
+        ConvertImageDtype(torch.float32),
         Resize(size=DEFINITION),
+        Normalize(mean=[0.5] * NUMBER_OF_CHANNELS, std=[0.5] * NUMBER_OF_CHANNELS),
     ]
 )
 
 ds_params = DatasetParams(
-    file_extension="npy",
+    file_extension="png",
     key_transform=int,
     sorting_func=lambda subdir: int(subdir.name),
-    dataset_class=NumpyDataset,
+    dataset_class=ImageDataset,
 )
 
-biotine_image_inference = DataSet(
-    name="biotine_image",
+dataset = DataSet(
+    name="biotine_png_hard_aug",
     data_shape=(NUMBER_OF_CHANNELS, DEFINITION, DEFINITION),
     transforms=transforms,
     selected_dists=list(range(1, 20)),  # not used
-    expected_initial_data_range=(0, 1),
+    expected_initial_data_range=(0, 255),
     dataset_params=ds_params,
 )
